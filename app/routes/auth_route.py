164e3f4bbd4fb850b5dd71ebbca5dce71346e9ed
Adlene <adlene.kermache@ensia.edu.dz>
2023-12-28 20:42:35 +0100

    d  version

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -8,12 +8,11 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/signup', methods=['POST'])
 def signup():
     supabase = connect_to_supabase()
-     
+
     data = request.form
-    
     email = data.get('email')
     password = data.get('password')
-    confirmPassword = data.get('confirmPassword')
+    confirm_password = data.get('confirmPassword')
     name = data.get('name')
     avatar = request.files.get('avatar')
     errors = []
@@ -35,7 +34,7 @@ def signup():
         errors.append('Email needs to be valid')
 
     # Validate password
-    password_error = is_valid_password(password, confirmPassword)
+    password_error = is_valid_password(password, confirm_password)
     if password_error:
         errors.append(password_error)
 
@@ -58,26 +57,25 @@ def signup():
             url = None
             
             if avatar:
+                # Upload the avatar image to Supabase storage
                 supabase.storage.from_("avatars").upload(
-                    file=avatar.read(),
-                    path=f'avatars/{user_id}/avatar',  # Adjust the path as needed
-                    file_options={"content-type": avatar.mimetype}
+                    file=avatar.read(),  # Read the content of the image file
+                    path=f'avatars/{user_id}/avatar',  # Specify the destination path in storage
+                    file_options={"content-type": avatar.mimetype}  # Set the content type of the file
                 )
-            
-                url = supabase.storage.from_('avatars').get_public_url(f'avatars/{user_id}/avatar')
-            
-            user_data = {'id': user_id, 'email': email, 'name': name, 'avatar_url': url}
-            supabase.table('users').insert(user_data).execute()
 
-            return jsonify({
-                'id': user_id,
-                'email': user_email,
-                'name': name,
-                'avatarUrl': url
-            }), 200
-        else:
-            return jsonify({'message': 'Signup failed! User information not found'}), 400
-        
+                # Get the public URL of the uploaded avatar image
+                url = supabase.storage.from_('avatars').get_public_url(f'avatars/{user_id}/avatar')
+
+                user_data = {'id': user_id, 'email': email, 'name': name, 'avatar_url': url}
+                supabase.table('users').insert(user_data).execute()
+
+        return jsonify({
+            'id': user_id,
+            'email': user_email,
+            'name': name,
+            'avatarUrl': url
+        }), 200
     except Exception as ex:
         return jsonify({'message': 'Signup failed', 'errors': [str(ex)]}), 400
 
@@ -87,22 +85,23 @@ def signin():
     supabase = connect_to_supabase()
     data = request.json
 
-    res = None
     try:
         res = supabase.auth.sign_in_with_password({'email': data.get('email'), 'password': data.get('password')})
     except Exception as ex:
-        return jsonify({'message': 'Login failed','errors': ex.args}), 400
+        return jsonify({'message': 'Login failed', 'errors': ex.args}), 400
 
+    user_info = res.user
+
+    if user_info:
+        user_id = user_info.id
+        user_email = user_info.email
+        response = supabase.table('users').select('name', 'avatar_url').eq('id', user_id).execute()
+
+        return jsonify({
+            'id': user_id,
+            'email': user_email,
+            'name': response.data[0]['name'],
+            'avatarUrl': response.data[0]['avatar_url']
+        }), 200
     else:
-        user_info = res.user
-
-        if user_info:
-            user_id = user_info.id # Access the 'id' attribute from 'user_info'
-            user_email = user_info.email  # Access the 'email' attribute from 'user_info'
-            response = supabase.table('users').select('name', 'avatar_url').eq('id', user_id).execute()
-            
-            return jsonify({
-                'id': user_id, 'email': user_email, 'name': response.data[0]['name'], 'avatarUrl': response.data[0]['avatar_url']
-            }), 200
-        else:
-            return jsonify({'message': 'Sign-in failed! User information not found'}), 400
+        return jsonify({'message': 'Sign-in failed! User information not found'}), 400
