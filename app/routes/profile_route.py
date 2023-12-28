50d9df14d37cddbf104e95c0a430325b73825a7f
Adlene <adlene.kermache@ensia.edu.dz>
2023-12-28 18:14:07 +0100

    3rd  version

--- app/routes/profile_route.py
+++ app/routes/profile_route.py
@@ -1,21 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.utils.database import connect_to_supabase
 
 # Create a blueprint for home routes
 profile_bp = Blueprint('profile', __name__)
 
 # Define the route and function
-
 @profile_bp.route('/get_avatar_url', methods=['GET'])
 def get_avatar_url():
     try:
         supabase = connect_to_supabase()
-        data = request.form
-        user_id = data.get('user_id')
+        user_id = request.args.get('user_id')
 
         # Query Supabase to get the avatar URL based on user ID
         result = supabase.from_('users').select('avatar_url').eq('id', user_id).execute()
-        
+
         if len(result['data']) > 0:
             avatar_url = result['data'][0]['avatar_url']
             return jsonify({'avatar_url': avatar_url}), 200
@@ -24,4 +22,3 @@ def get_avatar_url():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
