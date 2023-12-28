2db35cad606660fdae6b6d5440e0fe862b94ddca
Adlene <adlene.kermache@ensia.edu.dz>
2023-12-28 19:08:48 +0100

    4rd  version

--- app/routes/profile_route.py
+++ app/routes/profile_route.py
@@ -7,8 +7,10 @@ profile_bp = Blueprint('profile', __name__)
 # Define the route and function
 @profile_bp.route('/get_avatar_url', methods=['GET'])
 def get_avatar_url():
-    try:
-        supabase = connect_to_supabase()
+   return jsonify({'avatar_url': '18'})
+
+
+''' supabase = connect_to_supabase()
         user_id = request.args.get('user_id')
 
         # Query Supabase to get the avatar URL based on user ID
@@ -18,7 +20,6 @@ def get_avatar_url():
             avatar_url = result['data'][0]['avatar_url']
             return jsonify({'avatar_url': user_id}), 200
         else:
-            return jsonify({'error': 'User not found'}), 404
+            return jsonify({'error': 'User not found'}), 404'''
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    
