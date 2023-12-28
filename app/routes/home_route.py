b4d2cf3c9b91b7c5dd4a845731fa5862608ea272
Adlene <adlene.kermache@ensia.edu.dz>
2023-12-28 19:38:43 +0100

    d  version

--- app/routes/home_route.py
+++ app/routes/home_route.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 from app.utils.database import connect_to_supabase
+from flask import request
+
 
 # Create a blueprint for home routes
 bp = Blueprint('home', __name__)
@@ -7,5 +9,7 @@ bp = Blueprint('home', __name__)
 # Define the route and function
 @bp.route('/')
 def hello():
+
   user_id = request.args.get('user_id')
+  
   return jsonify({'id':user_id})
